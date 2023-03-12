494c5fc623d1948ceca957f0f6c4f43277b24212
MichaelHeinzman <seth@heinzman.net>
2023-03-11 21:10:43 -0500

    Updated packet_generator to not show my addresses.

--- app/network/packet_generator.py
+++ app/network/packet_generator.py
@@ -1,3 +1,5 @@
+__all__ = ['generate_packets']
+
 from packet_creator import create_packet
 from packet_sending import send_created_packets
 
@@ -6,10 +8,3 @@ from packet_sending import send_created_packets
 def generate_packets (packet_info):
     new_packet = create_packet(packet_info)
     send_created_packets(new_packet, packet_info)
-
-# Packet Info.
-IP_packet_info = {"type":"IP", "srcIP":"192.168.1.236", "dstIP":"192.168.1.236", "payload":"Hello", "number": 5}
-ARP_packet_info = {"type": "ARP-who-has", "pdst":"192.168.1.1", "hwdst":"ff:ff:ff:ff:ff:ff", "hwsrc": "00-05-1B-33-F7-46", "psrc": "192.168.1.236", "number": 1}
-
-# Call generate_packets to generate multiple packets based on the packet info passed.
-generate_packets(ARP_packet_info)
